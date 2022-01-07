93bc17f125ff78a52e18e6fc23734a658fee2156
Jan Susicky <susicky.jan@gmail.com>
2022-01-07 19:02:44 +0100

    netlify modules.json

--- tools/generators/github-workflows/index.ts
+++ tools/generators/github-workflows/index.ts
@@ -7,7 +7,7 @@ export default async function (tree: Tree, schema: Schema) {
   const projects = getProjects(tree);
   const mfes = Array.from(projects.entries()).filter(([name, config]) => (config as any).prefix === 'microfrontends');
 
-  const mfeNames = mfes.map(x => x[0]);
+  const mfeNames = mfes.map(x => x[0]).filter(x => x !== 'shell');
 
   for (const mfe of mfeNames) {
     generateFiles(tree, joinPathFragments(__dirname, './files'), '.github/workflows', {
