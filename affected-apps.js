87a5970b6f41d76c0eda5b288f383d4174aa2292
Jan Susicky <susicky.jan@gmail.com>
2022-01-10 12:55:06 +0100

    pipelines tweaks

--- affected-apps.js
+++ affected-apps.js
@@ -2,6 +2,7 @@ const { execSync } = require('child_process');
 
 const affected = execSync('npx nx affected:apps --plain', { encoding: 'utf-8' })
   .trim()
-  .split(' ');
+  .split(' ')
+  .filter(x => x.length > 0);
 
 console.log(JSON.stringify(affected));
--- tools/generators/github-workflows/index.ts
+++ tools/generators/github-workflows/index.ts
@@ -1,21 +1,36 @@
-import { formatFiles, Tree, generateFiles, getProjects, joinPathFragments } from '@nrwl/devkit';
-import { toClassName } from '@nrwl/workspace';
+import {
+  names,
+  formatFiles,
+  Tree,
+  generateFiles,
+  getProjects,
+  joinPathFragments,
+} from '@nrwl/devkit';
 
 interface Schema {}
 
 export default async function (tree: Tree, schema: Schema) {
   const projects = getProjects(tree);
-  const mfes = Array.from(projects.entries()).filter(([name, config]) => (config as any).prefix === 'microfrontends');
+  const mfes = Array.from(projects.entries()).filter(
+    ([name, config]) =>
+      config.projectType === 'application' &&
+      (config as any).prefix === 'microfrontends'
+  );
 
-  const mfeNames = mfes.map(x => x[0]).filter(x => x !== 'shell');
+  const mfeNames = mfes.map((x) => x[0]).filter((x) => x !== 'shell');
 
   for (const mfe of mfeNames) {
-    generateFiles(tree, joinPathFragments(__dirname, './files'), '.github/workflows', {
-      tmpl: '',
-      name: mfe,
-      nameUpper: mfe.toUpperCase(),
-      nameCapitalized: toClassName(mfe)
-    });
+    generateFiles(
+      tree,
+      joinPathFragments(__dirname, './files'),
+      '.github/workflows',
+      {
+        tmpl: '',
+        name: mfe,
+        nameUpper: mfe.toUpperCase(),
+        nameCapitalized: names(mfe).className,
+      }
+    );
   }
 
   await formatFiles(tree);
